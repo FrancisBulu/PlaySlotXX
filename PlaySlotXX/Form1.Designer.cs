d2f8e24f2f760f07d2da406b177092dd2ce7a3db
Francis Bulu <Francis.Bulu@student.intecbrussel.be>
2021-01-26 13:43:47 +0100

    Add project files.

---
+++ PlaySlotXX/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿
+namespace PlaySlotXX
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pic1 = new System.Windows.Forms.PictureBox();
+            this.pic2 = new System.Windows.Forms.PictureBox();
+            this.pic3 = new System.Windows.Forms.PictureBox();
+            this.lblAmount = new System.Windows.Forms.Label();
+            this.txtAmountInserted = new System.Windows.Forms.TextBox();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.imgList = new System.Windows.Forms.ImageList(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pic1
+            // 
+            this.pic1.Location = new System.Drawing.Point(38, 44);
+            this.pic1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pic1.Name = "pic1";
+            this.pic1.Size = new System.Drawing.Size(200, 208);
+            this.pic1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic1.TabIndex = 0;
+            this.pic1.TabStop = false;
+            // 
+            // pic2
+            // 
+            this.pic2.Location = new System.Drawing.Point(244, 44);
+            this.pic2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pic2.Name = "pic2";
+            this.pic2.Size = new System.Drawing.Size(200, 208);
+            this.pic2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic2.TabIndex = 1;
+            this.pic2.TabStop = false;
+            // 
+            // pic3
+            // 
+            this.pic3.Location = new System.Drawing.Point(451, 44);
+            this.pic3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pic3.Name = "pic3";
+            this.pic3.Size = new System.Drawing.Size(200, 208);
+            this.pic3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic3.TabIndex = 2;
+            this.pic3.TabStop = false;
+            // 
+            // lblAmount
+            // 
+            this.lblAmount.AutoSize = true;
+            this.lblAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAmount.Location = new System.Drawing.Point(38, 278);
+            this.lblAmount.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblAmount.Name = "lblAmount";
+            this.lblAmount.Size = new System.Drawing.Size(115, 20);
+            this.lblAmount.TabIndex = 3;
+            this.lblAmount.Text = "Amount to play";
+            // 
+            // txtAmountInserted
+            // 
+            this.txtAmountInserted.Location = new System.Drawing.Point(244, 278);
+            this.txtAmountInserted.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtAmountInserted.Name = "txtAmountInserted";
+            this.txtAmountInserted.Size = new System.Drawing.Size(201, 20);
+            this.txtAmountInserted.TabIndex = 4;
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(244, 313);
+            this.btnPlay.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(98, 37);
+            this.btnPlay.TabIndex = 5;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(346, 313);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(98, 37);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(274, 10);
+            this.lblTitle.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(138, 25);
+            this.lblTitle.TabIndex = 7;
+            this.lblTitle.Text = "Slot Machine";
+            // 
+            // imgList
+            // 
+            this.imgList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imgList.ImageStream")));
+            this.imgList.TransparentColor = System.Drawing.Color.Transparent;
+            this.imgList.Images.SetKeyName(0, "Ananas.jpg");
+            this.imgList.Images.SetKeyName(1, "Apples.jpg");
+            this.imgList.Images.SetKeyName(2, "Avocados.jpg");
+            this.imgList.Images.SetKeyName(3, "Bananas.jpg");
+            this.imgList.Images.SetKeyName(4, "Blueberries.jpg");
+            this.imgList.Images.SetKeyName(5, "Cherries.jpg");
+            this.imgList.Images.SetKeyName(6, "Grapes.jpg");
+            this.imgList.Images.SetKeyName(7, "Kiwis.jpg");
+            this.imgList.Images.SetKeyName(8, "Lemons.jpg");
+            this.imgList.Images.SetKeyName(9, "Mangoes.jpg");
+            this.imgList.Images.SetKeyName(10, "Melons.jpg");
+            this.imgList.Images.SetKeyName(11, "Oranges.jpg");
+            this.imgList.Images.SetKeyName(12, "Peaches.jpg");
+            this.imgList.Images.SetKeyName(13, "Pears.jpg");
+            this.imgList.Images.SetKeyName(14, "Strawberries.jpg");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MintCream;
+            this.ClientSize = new System.Drawing.Size(678, 364);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.txtAmountInserted);
+            this.Controls.Add(this.lblAmount);
+            this.Controls.Add(this.pic3);
+            this.Controls.Add(this.pic2);
+            this.Controls.Add(this.pic1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pic1;
+        private System.Windows.Forms.PictureBox pic2;
+        private System.Windows.Forms.PictureBox pic3;
+        private System.Windows.Forms.Label lblAmount;
+        private System.Windows.Forms.TextBox txtAmountInserted;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.ImageList imgList;
+    }
+}
+
---
+++ PlaySlotXX/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PlaySlotXX
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        decimal totalInserted = 0.00m;
+        decimal totalWinings = 0.00m;
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            decimal winingAmount = 0.00m;
+            decimal amountInserted = 0.00m;
+            if (decimal.TryParse(txtAmountInserted.Text, out amountInserted) && amountInserted > 0)
+            {
+
+                Random rand = new Random();
+                int pict_1 = rand.Next(0, 8);
+                int pict_2 = rand.Next(0, 8);
+                int pict_3 = rand.Next(0, 8);
+
+                pic1.Image = imgList.Images[pict_1];
+                pic2.Image = imgList.Images[pict_2];
+                pic3.Image = imgList.Images[pict_3];
+
+                totalInserted += amountInserted;
+                totalWinings += winingAmount = GetWiningAmount(pict_1, pict_2, pict_3, amountInserted); ;
+            }
+        }
+        public decimal GetWiningAmount(int p1, int p2, int p3, decimal amountInserted)
+        {
+            if (p1 == p2 && p2 == p3)
+            {
+                return amountInserted * 10;
+            }
+            else if (p1 == p2 || p2 == p3 || p1 == p3)
+            {
+                return amountInserted * 2;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Total inserted money: {totalInserted.ToString()} \n Total winings are: {totalWinings.ToString()}");
+        }
+    }
+}
